b041212b340c7fc3bc235fefe8f34b912bc1a014
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2021-12-01 18:32:21 +0100

    back to kotlin

---
+++ src/main/kotlin/pl/lo1workshop/demo/api/DemoApi.kt
@@ -0,0 +1,27 @@
+package pl.lo1workshop.demo.api
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DemoApi {
+
+    @GetMapping(
+        value = ["/", "/welcome"],
+        produces = [MediaType.TEXT_PLAIN_VALUE]
+    )
+    fun welcomeAll(): String {
+        return "Witajcie wszyscy na spotkaniu !"
+    }
+
+    @GetMapping(
+        value = ["/welcome/{name}"],
+        produces = [MediaType.TEXT_PLAIN_VALUE]
+    )
+    fun welcomeOne(@PathVariable("name") name: String): String {
+        return "Witaj ${name.replaceFirstChar{ it.uppercase()}} !!"
+    }
+
+}
